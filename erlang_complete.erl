0efac266fe2396f3936f6ee0c4bc6f5144efd214
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-01-08 00:41:30 +0100

    Includes in code path Rebar dependencies

--- erlang_complete.erl
+++ erlang_complete.erl
@@ -1,6 +1,14 @@
 #!/usr/bin/env escript
 
 main([ModuleName]) ->
+    code:add_path("ebin"),
+    case file:consult("rebar.config") of
+        {ok, Terms} ->
+            RebarDeps = proplists:get_value(deps_dir, Terms, "deps"),
+            code:add_paths(filelib:wildcard(RebarDeps ++ "/*/ebin"));
+        _ ->
+            ok
+    end,
     Module = erlang:list_to_atom(ModuleName),
     try Module:module_info(exports) of
         Functions ->
